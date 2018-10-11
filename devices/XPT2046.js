da935eba23eb0c61d2487d5b83a5a74d6e06a9bb
Markus Muetschard <markus@muet.com>
2018-10-11 08:48:17 -0700

    WIP - to figure out state

--- devices/XPT2046.js
+++ devices/XPT2046.js
@@ -1,18 +1,26 @@
-/* Copyright (c) 2013 Gordon Williams, Pur3 Ltd. and 2016 muet.com See the file LICENSE for copying permission. */
+/* Copyright (c) 2016 Markus Muetschard, markus@muet.com and 2013 Gordon Williams, Pur3 Ltd. See the file LICENSE for copying permission. */
 /*
 
 ----- XPT2046 ------------------------- 77 vars, 85 with calibrated calculation.
 
-Module for connecting to the XPT2046 resistive touchscreen controller (pin and funcgtion compatible to ADS7843).
+Module for connecting to the XPT2046 / ADS7843 / ... resistive touchscreen controllers. 
 
-The callback function has four arguments (X, Y, readData and module). When you move your finger on the touchscreen the X and Y coordinates are reported back, and when you lift your finger, the callback function is called once with X and Y and readData set to undefined.
+The XPT2046 is pin and function compatible to ADS7843 touch screen controller.
 
-Connect expects a calculation function that converts raw x and raw y to x and y. Calculation formula is rawValue / rawValuePerPixel + offset for x and y. Calculation arguments are rawX, rawY, readData and module. Calculation returns x, y, readData and module.
+The callback function has four arguments (X, Y, readData and module). When you
+move your finger on the touchscreen the X and Y coordinates are reported back,
+and when you lift your finger, the callback function is called once with X and
+Y and readData set to undefined.
+
+Connect expects a calculation function that converts raw x and raw y to x and
+y. Calculation formula is rawValue / rawValuePerPixel + offset for x and y.
+Calculation arguments are rawX, rawY, readData and module. Calculation returns
+x, y, readData and module.
 
 
 Usage
 
-Landscape format:
+Portrait format:
 
 ```
 SPI2.setup({sck:B13, miso:B14, mosi:B15, baud: 2000000});
@@ -21,8 +29,8 @@ var touch = require("XPT2046").connect(
       if (x !== undefined) {
         console.log( x + " @ " + y);
       }
-    }, function(yR, xR, d, m) { // portrait 240 x 320
-        return [ //rawVal / valPerPx * offset
+    }, function(yR, xR, m) { // portrait 240 x 320
+        return [ //rawVal / valPerPx + offset
             Math.round(xR / -121.44  + 259.707) 
           , Math.round(yR /   88.904 + -19.781)
           , d, m
@@ -47,7 +55,7 @@ Landscape format:
 
 ```
       ...  
-      }, function(xR, yR, d, m) { // landscape 320 x 240
+      }, function(xR, yR, m) { // landscape 320 x 240
           return [ //rawVal / valPerPx + offset
               Math.round(xR /  -88.904 +  339.781)
             , Math.round(yR / -121.44  +  259.707) 
@@ -56,51 +64,43 @@ Landscape format:
 
  */
 
-exports = // XPT2046 module - 
-{ poll: 50 // default poll interval when touched in milliseconds
-, calc: function(xRaw, yRaw, data, module) {  // default x / y
-      return [xRaw, yRaw, data, module];      // ...calculation...
-    }                                         // ... (pass thru)
+exports = // XPT2046 module
+{ scan: 50 // default scan interval for tracking moving touch
+, calc: function(xRaw, yRaw, module) {  // default x / y calc...
+      return [xRaw, yRaw, module]; // ...is just a pass through
+    }
 , lstn: false // is listening to touch and untouch (calling back)
-, listen:  function(lstn) {
-    this.lstn = ((lstn === undefined) || lstn);
-    return this;
-  }
-, connect: function(spi, cs, irq, callback, calc, poll) {
-    // overwrite default 'calculation' (pass thru raw values)
-    if (calc) { this.calc = calc; }
-    // overwrite default poll interval (50) in milliseconds
-    if (poll) { this.poll = poll; }
-    // wake the controller up
-    spi.send([0x90,0],cs);
-    // look for a press
-    var watchFunction = function() {
+, d: null // data as last read from XPT2046 touch controller
+, t: 0 // 0 = not touching, 1 = touch down, 3 = scan / tracking move
+, x: 0 // last (t=0)/current(t>0) x touch
+, y: 0 // last (t=0)/current(t>0) y touch
+, connect: function(spi, cs, irq, callback, calc, scan, _, u) {
+    _ = _ || this; // _ and u force shortening of code (performance?)
+    if (calc) { _.calc = calc; } // overwrite default pass thru calc
+    if (scan) { _.scan = scan; } // overwrite default scan of 50 [mx]
+    spi.send([0x90,0],cs); // wake the controller up
+    var watch = function() { // look for a press
       var interval = setInterval(function () {
-        if (!digitalRead(irq)) { // touch down
-          var data = spi.send([0x90,0,0xD0,0,0], cs);
-          if (this.lstn) {
-            callback.apply(
-                undefined
-              , this.calc(
-                    data[1]*256+data[2], data[3]*256+data[4]
-                  , data
-                  , this
-                  )
-              );
-          }
+        if (!digitalRead(irq)) { // touch/ing
+          _.t = (_.t) ? _.t |= 2 : 1;
+          var d = _.d = spi.send([0x90,0,0xD0,0,0], cs),
+              c = _.calc(d[1]*256+d[2], d[3]*256+d[4], _);
+          _.x = c[0]; _.y = c[1];
+          if (_.lstn) { callback.apply(u, c); }
         } else { // 'untouch'
           clearInterval(interval);
-          interval = undefined;
-          if (this.lstn) {
-            callback(undefined, undefined, undefined, this);
-          }
-          setWatch(watchFunction, irq
-              , { repeat : false, edge: "falling" });
+          _.t = 0;
+          interval = u;
+          if (_.lstn) { callback(u, u, _); }
+          setWatch(watch, irq, { repeat : false, edge: "falling" });
         }
-      }.bind(this), this.poll); // poll interval on touch
-    }.bind(this);
-    setWatch(watchFunction, irq
-        , { repeat : false, edge: "falling" });
+      }.bind(_), _.scan);
+    }.bind(_);
+    setWatch(watch, irq, { repeat : false, edge: "falling" });
+    return _;
+  }
+, listen:  function(lstn) {
+    this.lstn = ((lstn === undefined) || lstn);
     return this;
   }
 };
